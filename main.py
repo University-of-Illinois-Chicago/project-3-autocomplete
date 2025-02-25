90a5fb9c5b820c52e6f04a0c071fc416b3f6fd46
Givikap <kaitosekiya@outlook.com>
2025-02-25 09:47:06 -0600

    Modify .gitignore, add main.py with template code and header comment

---
+++ main.py
@@ -0,0 +1,28 @@
+#
+# Program 3: Autocomplete
+#
+# Course: CS 351, Spring 2025
+# System: MacOS using VS Code
+# Author: Kaito Sekiya
+# 
+# File: main.py
+#
+# Description TODO.
+# 
+# Usage: python main.py TODO
+# Example: python main.py TODO
+#
+
+##################################################################  
+#
+# main
+#
+# The controlling unit of the program. It prints majority of the text 
+# and calls the helper functions for the Autocomplete algorithm functionality.
+#
+def main() -> None:
+    pass
+
+# Just calls main function
+if __name__=="__main__":
+    main()
